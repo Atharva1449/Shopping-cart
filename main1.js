f2fc4c68272ace289e90e85477f6d6c2e26ca0ab
atharva_dawande <atharva.dawande603@gmail.com>
2021-06-17 10:47:25 +0530

    execution done ui remaining

--- main1.js
+++ main1.js
@@ -1,7 +1,15 @@
 var a=0;
-function fun(x){
+var b1;
+var aa="";
+function fun(x,name1){
 a=a+x;
+b1=a;
+
+aa=aa+"<br>"+name1;
+localStorage.setItem("key2",aa);
+
 //alert(a);
+localStorage.setItem("key",a);
 }
 
 function fun3(y){
@@ -9,3 +17,10 @@ function fun3(y){
     document.getElementById(y).style.backgroundColor='Pink';  
                    
 }
+
+function fun5(){
+    b1= localStorage.getItem("key2")+"<br><br>Total is:"+localStorage.getItem("key");
+
+    document.getElementById("parae").innerHTML=b1;
+}
+fun5();
